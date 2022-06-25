642fb6047a2d6012f779ec87e06418c8a3f23978
pillager-SB <78688006+pillager-SB@users.noreply.github.com>
2022-06-25 19:54:40 +0300

    create project

---
+++ geekshop/mainapp/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+
+# Create your views here.
+# Чтобы получить доступ к html файлам используем функциональный подход, создаем контроллер:
+def index(request):
+    return render(request, 'mainapp/index.html')
+
+def products(request):
+    return render(request, 'mainapp/products.html')
